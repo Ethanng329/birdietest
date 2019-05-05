359a8472a88ec0460959b3b55c9a9e9864dca68f
Ethan Ng <ethanng329@gmail.com>
2019-05-05 15:51:31 +0100

    DB query working

--- server.js
+++ server.js
@@ -7,6 +7,34 @@ require('dotenv').config({
   path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}`)
 });
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+connection.query(`SELECT * from ${process.env.DB_TABLE}`, function(
+  err,
+  rows,
+  fields
+) {
+  if (!err) {
+    console.log('result', rows, fields);
+  } else console.log(err);
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
@@ -14,6 +42,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(process.env.PORT || 8080, function() {
-  
   console.log('Listening to porting 8080');
 });
